073f597f845e26945235bcf5072f42ac889ab907
nyehamene <nhcdeveloper@gmail.com>
2025-06-05 22:12:26 +0100

    update highlight; highlight comment

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,15 +1,19 @@
 (production_definition
-  name: (identifier) @entity.name.function @function
-  ) @meta.function @function
+  name: (identifier) @entity.name.function
+  ) @function
 
 (regexp
   ; open: "/" @punctuation.definition.regexp.begin
   ; close: "/" @punctuation.definition.regexp.end
-  ) @string.regex
+  ) @string.regexp
 
 (string
   ; open: "\"" @punctuation.definition.string.begin
   ; close: "\"" @punctuation.definition.string.end
   ) @string.quoted.double
 
-(identifier) @variable.function
+(identifier !open) @variable.function
+
+(identifier open: "$") @variable.function.global
+
+(comment) @comment
