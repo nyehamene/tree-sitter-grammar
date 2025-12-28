d1dc265b8c222ebf80aa387233713b1652a0013e
nyehamene <nhcdeveloper@gmail.com>
2025-12-28 13:24:56 +0100

    tags

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,19 +1,8 @@
 ;; Tag queries for the grammar language
 
 ;; Definitions
-(rule
-  name: (ident) @definition.rule
-)
-
-(binding
-  name: (ident) @definition.binding
-)
-
-
-(member_access
-  object: (ident) @reference
-)
-
-(member_access
-  property: (ident) @reference
-)
+(rule          name:     (ident) @name) @definition.method
+(binding       name:     (ident) @name) @reference.class
+(member_access object:   (ident) @name) @reference.class
+(member_access property: (ident) @name) @reference.call
+((ident) @name)                         @reference.call
